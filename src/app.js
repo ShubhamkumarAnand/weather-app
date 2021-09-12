4e669e662c662ea552810e75536b0b986db6a621
imskanand <the.anand3099@gmail.com>
2021-09-12 20:44:05 +0530

    Web Servers completed

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 const port = 3000
 
 // Define path foe Express config
 const pathToDirectory = path.join(__dirname, '../public')
-const pathToViews = path.join(__dirname,'../views')
+const pathToViews = path.join(__dirname,'../templates/views')
+const pathToPartials = path.join(__dirname,'../templates/partials')
 
 // Setup handlebars engine & views location
 app.set('view engine', 'hbs')
 app.set('views', pathToViews)
+hbs.registerPartials(pathToPartials)
 
 // Setup static Directory to serve
 app.use(express.static(pathToDirectory))
@@ -31,13 +34,33 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
   res.render('help', {
+    service: 'what i can help you with!',
     title: 'Help',
-    service: 'what i can help you with!'
+    name: 'imskanand'
   })
 })
 
 app.get('/weather', (req, res) => {
-  res.send()
+  res.send({
+    location: 'Patna',
+    Condition: 'Hottest Day 🔥'
+  })
+})
+
+app.get('/help/*', (req, res) => {
+  res.send('404', {
+    title: 'Help Page Not found',
+    errorMessage: 'Help Article Not Found!',
+    name: 'imskanand'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404 Not Found',
+    name: 'imskanand',
+    errorMessage:'Page Not Available'
+  })
 })
 
 app.listen(port, () => {
