510ccd0506a1bcbe539c4a19797705b272017349
imskanand <the.anand3099@gmail.com>
2021-09-12 19:58:47 +0530

    Dynamic Templating

--- src/app.js
+++ src/app.js
@@ -4,9 +4,37 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// Define path foe Express config
 const pathToDirectory = path.join(__dirname, '../public')
+const pathToViews = path.join(__dirname,'../views')
 
+// Setup handlebars engine & views location
+app.set('view engine', 'hbs')
+app.set('views', pathToViews)
+
+// Setup static Directory to serve
 app.use(express.static(pathToDirectory))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'imskanand'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    name: 'imskanand'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    service: 'what i can help you with!'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send()
